4372f8a16c436dcccd92147b68bbccb608dcd314
emil <emilmourad8@gmail.com>
2022-02-15 21:10:27 +0200

    my first commit

---
+++ Pages/test.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h1>Products</h1>
+
+<font>
+
+<form action="/action_page.php"> <b>
+
+  <label for="fname">Product name:</label><br> <b>
+  <input type="text" id="fname" name="fname" value="write here"><br> <br>
+  
+  <label for="fname">Category name:</label><br> <b>
+  <input type="text" id="fname" name="fname" value="write here"><br> <br>
+  
+  <label for="fname">Product image link:</label><br> <b>
+  <input type="text" id="fname" name="fname" value="write here"><br> <br>
+ 
+  <label for="fname">Manifacture name:</label><br> 
+  <input type="text" id="fname" name="fname" value="write here"><br> <br>
+  
+  <label for="fname">Technical spes:</label><br> 
+  <input type="text" id="fname" name="fname" value="write here"><br> <br>
+ 
+  <label for="fname">Price double:</label><br> 
+  <input type="text" id="fname" name="fname" value="write here"><br> <br>
+  
+  <label for="fname">Quantity Avalilable New INT:</label><br> 
+  <input type="text" id="fname" name="fname" value="write here"><br> <br>
+  
+  <label for="fname">Quantity Avalilable Ref INT:</label><br> 
+  <input type="text" id="fname" name="fname" value="write here"><br> <br>
+  
+  <label for="fname">Discount Double:</label><br> 
+  <input type="text" id="fname" name="fname" value="write here"><br> <br>
+
+
+<label for="the form above">Select if the product is new or inserted before: </label>
+<select id="=">
+  <option value="">--Make a choice--</option>
+  <option value="new">New Product</option>
+  <option value="old">Old product</option>
+</select>
+<p></p>
+ 
+ 
+<input type="submit" value="Submit"
+       style="background-color:#008080; 
+              border: solid 4px #000000;
+              height: 45px; 
+              font-size:25px; 
+              vertical-align:20px" />
+              
+
+</form> 
+ 
+</body>
+
+</html>
---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+// require MySQL Connection
+require ('database/connection.php');
+
+// require Product Class
+require ('database/data.php');
+
+require ('database/search.php');
+
+require ('database/cart.php');
+
+require ('database/favourite.php');
+
+require ('database/returnProd.php');
+
+require ('database/userBalance.php');
+
+require ('database/checkout.php');
+
+require ('database/addCusShipping.php');
+
+// returnProduct ( 1 , 'Samsung Galaxy 10' , 1 ) ;
